0c05a54b7bd250f264ffeef422c0ab6c83ea3f68
Tiago Martins <101006760+ThStartBoss@users.noreply.github.com>
2022-11-25 12:50:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function Criar(){
+    alert('Sua conta foi criada!')
+    nome = document.getElementById("nome").value = ""
+    email = document.getElementById("email").value = ""
+    senha = document.getElementById("senha").value = ""
+    
+}
+
+function enviar(){
+        alert('O codigo foi enviado para o seu email')
+}
